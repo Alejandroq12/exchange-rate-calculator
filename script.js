9e496d0813bb48e47206ef6b6882f246ae21e71c
Julio Quezada <julio.quezada@catolica.edu.sv>
2023-04-24 20:31:16 -0600

    Add swap button

--- script.js
+++ script.js
@@ -30,4 +30,11 @@ amountEl_one.addEventListener('input', calculate);
 currencyEl_two.addEventListener('change', calculate);
 amountEl_two.addEventListener('input', calculate);
 
+swap.addEventListener('click', () => {
+  const temp = currencyEl_one.value;
+  currencyEl_one.value = currencyEl_two.value;
+  currencyEl_two.value = temp;
+
+  calculate();
+});
 calculate();
