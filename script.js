3831a07a3fa23daf7cdb09da13658888a16b47f5
Julio Quezada <julio.quezada@catolica.edu.sv>
2023-04-24 19:21:34 -0600

    Add event listeners

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
+const currencyEl_one = document.getElementById('currency-one');
+const amountEl_one = document.getElementById('amount-one');
+const currencyEl_two = document.getElementById('currency-two');
+const amountEl_two = document.getElementById('amount-two');
+
+const rateEl = document.getElementById('rate');
+const swap = document.getElementById('swap');
+
+// Fetch exchange rates and update the DOM
 function calculate() {
-    
+
 }
 
 calculate();
